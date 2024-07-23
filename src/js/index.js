3fa1028118ac635ad737c643db71575117cadb6a
DrunkBelligerentWizard <j.c.vandagrift@gmail.com>
2024-07-23 01:05:55 -0700

    Implemented snow fall speed based off of window height

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import config from '../assets/particles.json'
 import 'particles.js'
 import '../css/index.css';
 
-console.log(config)
-particlesJS("particles-js", { "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 200 } }, "color": { "value": "#fff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": true, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 8, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": false, "distance": 500, "color": "#ffffff", "opacity": 0.4, "width": 2 }, "move": { "enable": true, "speed": 3.5, "direction": "bottom", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 10000, "rotateY": 10000 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": false, "mode": "bubble" }, "onclick": { "enable": false, "mode": "bubble" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 0.5 } }, "bubble": { "distance": 400, "size": 4, "duration": 0.3, "opacity": 1, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": false });
+config.particles.move.speed = parseFloat((window.outerHeight / 385).toFixed(2))
+console.log(config.particles.move.speed)
+particlesJS("particles-js", config);
