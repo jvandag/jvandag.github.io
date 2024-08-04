a8b43c15e458467caf34020e05c47303f07d8480
DrunkBelligerentWizard <j.c.vandagrift@gmail.com>
2024-08-03 19:19:46 -0700

    feat: reformatted navbar, added projects page

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,38 @@ import config from '../assets/particles.json'
 import 'particles.js'
 import '../css/index.css';
 
+
+function parseUA() {
+    var u = navigator.userAgent;
+    var u2 = navigator.userAgent.toLowerCase();
+    return { 
+        trident: u.indexOf('Trident') > -1, 
+        presto: u.indexOf('Presto') > -1, 
+        webKit: u.indexOf('AppleWebKit') > -1, 
+        gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, 
+        mobile: !!u.match(/AppleWebKit.*Mobile.*/), 
+        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), 
+        android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, 
+        iPhone: u.indexOf('iPhone') > -1, 
+        iPad: u.indexOf('iPad') > -1, 
+        webApp: u.indexOf('Safari') == -1, 
+        iosv: u.substr(u.indexOf('iPhone OS') + 9, 3),
+        weixin: u2.match(/MicroMessenger/i) == "micromessenger",
+        ali: u.indexOf('AliApp') > -1,
+    };
+}
+var ua = parseUA();
+var metaTag=document.createElement('meta');
+metaTag.name = "viewport"
+if (ua.mobile) {
+    metaTag.content = "width=1024, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+}
+else {
+    metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+}
+
+document.getElementsByTagName('head')[0].appendChild(metaTag);
+
 const createBionicText = () => {
     let textElems = document.getElementsByClassName("bionicText");
     console.log(textElems)
@@ -32,9 +64,11 @@ const adjustContentCSS = () => {
         //not webkit based
         //remove text clip color if not webkitbased
         let content = document.getElementById("contentContainer")
-        content.style.backgroundImage = 'none'
-        content.style.background = 'none'
-        content.style.color = 'black'
+        if (content) {
+            content.style.backgroundImage = 'none'
+            content.style.background = 'none'
+            content.style.color = 'black'
+        }
     }
     
 }
