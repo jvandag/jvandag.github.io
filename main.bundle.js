f451e0ab109e3621f7aa694a32b5fc694c441f4a
DrunkBelligerentWizard <j.c.vandagrift@gmail.com>
2024-08-03 19:25:26 -0700

    Updates

--- main.bundle.js
+++ main.bundle.js
@@ -1661,6 +1661,34 @@ function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length)
 
 
 
+function parseUA() {
+  var u = navigator.userAgent;
+  var u2 = navigator.userAgent.toLowerCase();
+  return {
+    trident: u.indexOf('Trident') > -1,
+    presto: u.indexOf('Presto') > -1,
+    webKit: u.indexOf('AppleWebKit') > -1,
+    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,
+    mobile: !!u.match(/AppleWebKit.*Mobile.*/),
+    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+    android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1,
+    iPhone: u.indexOf('iPhone') > -1,
+    iPad: u.indexOf('iPad') > -1,
+    webApp: u.indexOf('Safari') == -1,
+    iosv: u.substr(u.indexOf('iPhone OS') + 9, 3),
+    weixin: u2.match(/MicroMessenger/i) == "micromessenger",
+    ali: u.indexOf('AliApp') > -1
+  };
+}
+var ua = parseUA();
+var metaTag = document.createElement('meta');
+metaTag.name = "viewport";
+if (ua.mobile) {
+  metaTag.content = "width=1024, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
+} else {
+  metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
+}
+document.getElementsByTagName('head')[0].appendChild(metaTag);
 var createBionicText = function createBionicText() {
   var textElems = document.getElementsByClassName("bionicText");
   console.log(textElems);
@@ -1696,9 +1724,11 @@ var adjustContentCSS = function adjustContentCSS() {
     //not webkit based
     //remove text clip color if not webkitbased
     var content = document.getElementById("contentContainer");
-    content.style.backgroundImage = 'none';
-    content.style.background = 'none';
-    content.style.color = 'black';
+    if (content) {
+      content.style.backgroundImage = 'none';
+      content.style.background = 'none';
+      content.style.color = 'black';
+    }
   }
 };
 adjustContentCSS();
