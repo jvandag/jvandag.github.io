ac0d01ab3661c780464bb343284b79457810a8cb
DrunkBelligerentWizard <j.c.vandagrift@gmail.com>
2024-08-05 02:16:53 -0700

    Updates

--- main.bundle.js
+++ main.bundle.js
@@ -1721,6 +1721,25 @@ var adjustContentCSS = function adjustContentCSS() {
   if (contentBG) {
     contentBG.style.backgroundImage = "linear-gradient(transparent ".concat(offset - 1, "px, rgba(205, 205, 226, 0.75) calc(").concat(offset, "px + 3.5rem), rgba(205, 205, 226, 0.75) calc(100% - 3.5rem ), transparent calc(100% - 0.5rem))");
   }
+  var aboutTile = document.getElementById("aboutTile");
+  if (aboutTile) {
+    window.addEventListener("resize", function () {
+      //the point at which the about tile will overlap with the navbar
+      var height = offset * 2 + aboutTile.offsetHeight + 5;
+      if (window.innerHeight <= height) {
+        aboutTile.style.position = 'fixed';
+        aboutTile.style.top = "".concat(offset + 5, "px");
+        aboutTile.style.right = "calc(50% - ".concat(aboutTile.offsetWidth / 2, "px)");
+      } else {
+        aboutTile.style.position = 'relative';
+        aboutTile.style.top = "0px";
+        aboutTile.style.right = "0px";
+      }
+    });
+    //check resize condition on page load
+    var event = new Event('resize');
+    window.dispatchEvent(event);
+  }
   if (navigator.userAgent.indexOf('AppleWebKit') === -1) {
     //not webkit based
     //remove text clip color if not webkitbased
